21c7ff2cf6dc31ff322ded521d56a8ceda0039a6
Tanbir Ahammad <73418055+tanbirahammad@users.noreply.github.com>
2025-03-27 14:23:18 +0600

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,17 +1,19 @@
-import profileImg from '../../assets/images/profile.png'
+import profileImg from '../../assets/images/profile.png';
 
 const Header = () => {
-    return (
-        <>
-        <div className='flex justify-between items-center p-4 border-b-2'>
-        <h1 className="text-4xl font-bold">Knowledge Cafe</h1>
-        <img src={profileImg} alt="" srcset="" />
-        
-        </div>
-       
-            
-        </>
-    );
+  return (
+    <div className="flex justify-between items-center p-6 border-b-2 border-gray-200 shadow-md bg-white">
+      {/* Website Title */}
+      <h1 className="text-4xl font-extrabold text-gray-900">Knowledge Cafe</h1>
+
+      {/* Profile Image */}
+      <img
+        src={profileImg}
+        alt="Profile"
+        className="w-12 h-12 rounded-full object-cover border-2 border-blue-500"
+      />
+    </div>
+  );
 };
 
 export default Header;
